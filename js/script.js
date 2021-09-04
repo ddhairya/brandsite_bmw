58f99426e8d56cfb126d5a4d9aabdc421f6f774d
Dhairya Doshi <d_dhairya@yahoo.com>
2021-09-04 18:45:54 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 gsap.registerPlugin(ScrollTrigger);
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-// tl.to(".text-right", { y: "0%", duration: 1, stagger: 0.25 });
-// tl.to(".hide-left", { x: "0%", duration: 0.75 },"-=0.75");
-// tl.to(".text-left", { y: "0%", duration: 0.75, stagger: 0.25 },"-=0.5");
-tl.to(".intro", { y: "-100%", duration: 1, delay: 0.25  });
+tl.to(".text-right", { y: "0%", duration: 1, stagger: 0.25 });
+ tl.to(".hide-left", { x: "0%", duration: 0.75 },"-=0.75");
+tl.to(".text-left", { y: "0%", duration: 0.75, stagger: 0.25 },"-=0.5");
+tl.to(".intro", { y: "-100%", duration: 1, delay: 0.5  });
 // tl.to(".intro", { y: "-100%", duration: 1, delay: 0.5  });
 
 
